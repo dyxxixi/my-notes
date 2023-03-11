f209d64a24ef8baab7d4361cfdb4b2fb393dfd30
d-yx <352104389@qq.com>
2023-03-11 16:34:47 +0800

    feat:add frontend-interview-javascript

--- index.js
+++ index.js
@@ -24,6 +24,101 @@ new Docute({
   //
   sidebar: [
     {
+      title: "JS面试",
+      children: [
+        {
+          title: "let、var、const的区别",
+          link: "/frontend-interview-javascript/01",
+        },
+        {
+          title: "值和引用",
+          link: "/frontend-interview-javascript/02",
+        },
+        {
+          title: "包装类型",
+          link: "/frontend-interview-javascript/03",
+        }, {
+          title: "数据类型的转换",
+          link: "/frontend-interview-javascript/04",
+        }, {
+          title: "运算符",
+          link: "/frontend-interview-javascript/05",
+        }, {
+          title: "原型和原型链",
+          link: "/frontend-interview-javascript/06",
+        }, {
+          title: "执行栈和执行上下文",
+          link: "/frontend-interview-javascript/07",
+        }, {
+          title: "作用域和作用域链",
+          link: "/frontend-interview-javascript/08",
+        }, {
+          title: "this指向",
+          link: "/frontend-interview-javascript/09",
+        }, {
+          title: "垃圾回收与内存泄漏",
+          link: "/frontend-interview-javascript/10",
+        }, {
+          title: "闭包",
+          link: "/frontend-interview-javascript/11",
+        }, {
+          title: "事件的注册和移除",
+          link: "/frontend-interview-javascript/12",
+        }, {
+          title: "DOM事件的传播机制",
+          link: "/frontend-interview-javascript/13",
+        }, {
+          title: "阻止事件默认行为",
+          link: "/frontend-interview-javascript/14",
+        }, {
+          title: "递归",
+          link: "/frontend-interview-javascript/15",
+        }, {
+          title: "属性描述符",
+          link: "/frontend-interview-javascript/16",
+        }, {
+          title: "class和构造函数区别",
+          link: "/frontend-interview-javascript/17",
+        }, {
+          title: "浮点数精度问题",
+          link: "/frontend-interview-javascript/18",
+        }, {
+          title: "严格模式",
+          link: "/frontend-interview-javascript/19",
+        },
+        {
+          title: "函数防抖和节流",
+          link: "/frontend-interview-javascript/20",
+        },
+        {
+          title: "WeakSet和WeakMap",
+          link: "/frontend-interview-javascript/21",
+        }, {
+          title: "深浅拷贝",
+          link: "/frontend-interview-javascript/22",
+        }, {
+          title: "函数柯里化",
+          link: "/frontend-interview-javascript/23",
+        }, {
+          title: "Node事件循环",
+          link: "/frontend-interview-javascript/24",
+        }, {
+          title: "eval",
+          link: "/frontend-interview-javascript/25",
+        }, {
+          title: "尺寸和位置",
+          link: "/frontend-interview-javascript/26",
+        }, {
+          title: "更多知识",
+          link: "/frontend-interview-javascript/27",
+        },
+        {
+          title: "JS面试题汇总",
+          link: "/frontend-interview-javascript/28",
+        },
+      ],
+    },
+    {
       title: "个人博客系统",
       children: [
         {
